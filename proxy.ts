0b59f31fb9c3ef3fef388400ec40a7639a9eafb4
Hadiru Colley <dirudeen22@gmail.com>
2026-02-21 03:57:16 +0000

    feat: Update proxy matcher to simplify request path exclusions

--- proxy.ts
+++ proxy.ts
@@ -15,14 +15,5 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
-     */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
+  matcher: ["/((?!api|_next|static|login|favicon.ico).*)"],
 };
