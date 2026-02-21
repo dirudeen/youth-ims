abe6df068d7bdd70e0e0e1362a799d00f2e588f5
Hadiru Colley <dirudeen22@gmail.com>
2026-02-21 03:03:46 +0000

    feat: Add database configuration and user schema for PostgreSQL integration

---
+++ db/index.ts
@@ -0,0 +1,11 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { env } from "@/env";
+import * as schema from "@/db/schema";
+import * as authSchema from "@/auth-schema";
+
+export const db = drizzle(env.DATABASE_URL, {
+  schema: { ...schema, ...authSchema },
+});
+// export const db = drizzle(env.DATABASE_URL, {
+//   schema: { ...schema },
+// });
