ebc0dd89dac386e652b90826d4ad76d0be562cc9
Hadiru Colley <dirudeen22@gmail.com>
2026-02-23 17:13:49 +0000

    feat: create youth-population-actions.tsx

---
+++ app/(dashboard)/youth-population/_components/youth-population-actions.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { CanEditWrapper } from "@/components/can-edit-wrapper";
+import { DataImportDialog } from "@/components/data-import-dialog";
+import { Dialog } from "@/components/ui/dialog";
+import { useYouthPopulationStore } from "@/store/youth-population-store";
+import { DialogTrigger } from "@radix-ui/react-dialog";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
+export default function YouthPopulationActions() {
+  const { createOpen, setCreateOpen } = useYouthPopulationStore();
+  return (
+    <div className="flex gap-2">
+      <DataImportDialog tableName="youth_population" />
+      <Dialog open={createOpen} onOpenChange={setCreateOpen}>
+        <DialogTrigger asChild>
+          <Button className="flex items-center gap-1" variant="outline">
+            <PlusCircle className="mr-2 h-4 w-4" />
+            Add New Entry
+          </Button>
+        </DialogTrigger>
+      </Dialog>
+    </div>
+  );
+}
